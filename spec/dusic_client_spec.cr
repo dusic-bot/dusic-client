af4f1fb52433f95dff52e0914bc1c05b0a8b43a7
Vladislav Kuznecov <fizvlad@mail.ru>
2021-04-28 23:19:23 +0300

    fix: tests

--- spec/dusic_client_spec.cr
+++ spec/dusic_client_spec.cr
@@ -2,8 +2,4 @@ require "./spec_helper"
 
 describe DusicClient do
   # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
-  end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/dusic-client"
+require "../src/dusic_client"
