04f2e2def814842462d7ad32d5fa3f0b937cccf4
Vladislav Kuznecov <fizvlad@mail.ru>
2021-05-06 15:34:25 +0300

    test: cover .read and .write

--- spec/secrets_spec.cr
+++ spec/secrets_spec.cr
@@ -12,4 +12,28 @@ Spectator.describe Secrets do
       expect(subject["answer"]).to eq(42)
     end
   end
+
+  describe ".read" do
+    subject { described_class.read(environment) }
+
+    it do
+      is_expected.to be_a(String)
+      is_expected.not_to be_blank
+    end
+  end
+
+  describe ".write" do
+    subject(call) { described_class.write(data, environment) }
+
+    let(data) { "data" }
+
+    it do
+      pre = described_class.read(environment)
+      expect(pre).not_to be_blank
+      call
+      post = described_class.read(environment)
+      expect(post).to eq(data)
+      described_class.write(pre, environment)
+    end
+  end
 end
