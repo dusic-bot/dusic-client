cd8bb60517b5a42f66bf9b144bfe30999ef8132c
Vladislav Kuznecov <fizvlad@mail.ru>
2021-05-11 02:21:07 +0300

    feat: CommandCall

--- src/worker/command_call.cr
+++ src/worker/command_call.cr
@@ -1,5 +1,11 @@
 class Worker
   # Data about single command call
-  class CommandCall
+  struct CommandCall
+    getter name : String
+    getter arguments : Array(String)
+    getter options : Hash(String, String?)
+
+    def initialize(@name, @arguments, @options)
+    end
   end
 end
