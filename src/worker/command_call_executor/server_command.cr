f4fa1bd2b646f09af769a04b675fadeed281da80
Vladislav Kuznecov <fizvlad@mail.ru>
2021-05-24 19:43:44 +0300

    feat: server command

--- src/worker/command_call_executor/server_command.cr
+++ src/worker/command_call_executor/server_command.cr
@@ -4,8 +4,27 @@ class Worker
   class CommandCallExecutor
     class ServerCommand < Base
       def execute
-        Log.debug { "Command #{self.class}" }
-        # TODO
+        if @command_call.server_id.zero?
+          reply(
+            t("commands.server.title"),
+            t("commands.server.errors.only_available_in_guild_channels"),
+            "danger"
+          )
+          return
+        end
+
+        reply(
+          t("commands.server.title"),
+          t("commands.server.text.statistic", {
+            guild_id:            @command_call.server_id,
+            total_tracks_length: Dusic.format_seconds(server.statistic.tracks_length),
+            total_tracks_amount: server.statistic.tracks_amount,
+            daily_tracks_length: Dusic.format_seconds(server.today_statistic.tracks_length),
+            daily_tracks_amount: server.today_statistic.tracks_amount,
+            today:               server.today_statistic.date.to_s("%d.%m.%y"),
+          }),
+          "success"
+        )
       end
     end
   end
