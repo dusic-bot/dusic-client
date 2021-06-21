d572165f5c73752ae30aa120006749fe603242fb
Vladislav Kuznecov <fizvlad@mail.ru>
2021-06-21 17:09:14 +0300

    feat: better skip scenarios

--- src/worker/command_call_executor/skip_command.cr
+++ src/worker/command_call_executor/skip_command.cr
@@ -5,6 +5,13 @@ class Worker
     class SkipCommand < Command
       def execute : Nil
         initial_size = audio_player.queue.size
+        is_currently_playing = !audio_player.current_audio.nil?
+
+        if initial_size.zero? && !is_currently_playing
+          reply(t("commands.skip.title"), t("commands.skip.text.nothing_is_playing"), "success")
+          return
+        end
+
         begin
           argument = @command_call.arguments.empty? ? 1 : @command_call.arguments.join.to_i32
           raise "Out of borders" if argument <= 0
@@ -16,7 +23,9 @@ class Worker
           return
         end
 
-        skipped_count = initial_size - audio_player.queue.size + 1
+        skipped_count = initial_size - audio_player.queue.size
+        skipped_count += is_currently_playing ? 1 : 0
+
         reply(t("commands.skip.title"), t("commands.skip.text.tracks_skipped", count: skipped_count), "success")
       end
     end
