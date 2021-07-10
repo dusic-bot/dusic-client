dfc3bc4ebdf1204d743b7cc983095864eec87f78
Vladislav Kuznecov <fizvlad@mail.ru>
2021-07-11 01:14:14 +0300

    fix: tests

--- spec/worker/command_call_spec.cr
+++ spec/worker/command_call_spec.cr
@@ -19,18 +19,18 @@ Spectator.describe Worker::CommandCall do
   describe "#to_s" do
     subject { instance.to_s }
 
-    it { is_expected.to eq("`name`[\"arg1\", \"arg2\"]{\"opt1\" => \"val1\", \"opt2\" => nil}") }
+    it { is_expected.to eq("`name`[\"arg1\", \"arg2\"]{\"opt1\" => \"val1\", \"opt2\" => nil} at 0 by 1") }
 
     context "when no args" do
       let(arguments) { [] of String }
 
-      it { is_expected.to eq("`name`[]{\"opt1\" => \"val1\", \"opt2\" => nil}") }
+      it { is_expected.to eq("`name`[]{\"opt1\" => \"val1\", \"opt2\" => nil} at 0 by 1") }
     end
 
     context "when no options" do
       let(options) { {} of String => String? }
 
-      it { is_expected.to eq("`name`[\"arg1\", \"arg2\"]{}") }
+      it { is_expected.to eq("`name`[\"arg1\", \"arg2\"]{} at 0 by 1") }
     end
   end
 end
