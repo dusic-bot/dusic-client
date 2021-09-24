c2288aa45313c959efc52b4032bcd9261b5133df
Vladislav Kuznecov <fizvlad@mail.ru>
2021-09-24 14:05:06 +0300

    docs: describe abstract methods

--- src/worker/discord_client/voice_client.cr
+++ src/worker/discord_client/voice_client.cr
@@ -2,10 +2,15 @@ class Worker
   class DiscordClient
     # Discord voice client facade
     abstract class VoiceClient
+      # Returns current frame (originally 'frame' is 20 ms of playback)
       abstract def current_frame : UInt64
+      # VSU event handler
       abstract def voice_server_update(endpoint : String, token : String, session_id : String) : Nil
+      # Setup WS close event listener
       abstract def on_close(&on_close : ->) : Nil
+      # Play single track
       abstract def play(audio : AudioPlayer::Audio, skip_frames : UInt64 = 0_u64) : Nil
+      # Stop playback
       abstract def stop : Nil
     end
   end
